37e8bf604836e113ec28f73baa4577e63fe6f4dd
csrap <caugusto3110@gmail.com>
2020-12-04 19:53:33 -0300

    Flask_2

---
+++ movements/views.py
@@ -0,0 +1,24 @@
+from movements import app
+from flask import render_template
+
+import csv
+
+@app.route('/')
+
+def listaIngresos():
+    fIngresos = open("movements/data/basededatos.csv", "r")
+    csvReader = csv.reader(fIngresos, delimiter=',', quotechar='"')
+    ingresos = list(csvReader)
+
+    print(ingresos)
+
+    total = 0
+    for ingreso in ingresos:
+        total += float(ingreso[2])
+
+    return render_template("movementsList.html",datos=ingresos, total=total)
+
+
+@app.route('/creaalta')
+def nuevoIngreso():
+    return 'Miercoles'
